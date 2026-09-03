d78b88fb9fe68c609fbb595e1beae51fa47ab678
ArgonautGit <nick-github@vansid.org>
2026-09-02 21:43:58 -0400

    config: before rebuild 2026-09-02 21:43
    added yazi bookmarks plugin

--- modules/yazi/yazi.nix
+++ modules/yazi/yazi.nix
@@ -1,9 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home-manager.users.nick = {
     programs.yazi = {
       enable = true;
       enableBashIntegration = true;
+
+      plugins = {
+        bookmarks = pkgs.yaziPlugins.bookmarks;
+      };
+
+      initLua = ''
+        require("bookmarks"):setup({
+          last_directory = { enable = false },
+          persist = "vim",
+          desc_format = "full",
+          notify = { enable = true },
+        })
+      '';
 
       keymap = {
         mgr.prepend_keymap = [
@@ -11,6 +24,26 @@
             on = "!";
             run = ''shell "$SHELL" --block'';
             desc = "Open shell here";
+          }
+          {
+            on = "m";
+            run = "plugin bookmarks save";
+            desc = "Save current position as a mark";
+          }
+          {
+            on = "'";
+            run = "plugin bookmarks jump";
+            desc = "Jump to a mark";
+          }
+          {
+            on = [ "b" "d" ];
+            run = "plugin bookmarks delete";
+            desc = "Delete a mark";
+          }
+          {
+            on = [ "b" "D" ];
+            run = "plugin bookmarks delete_all";
+            desc = "Delete all marks";
           }
         ];
       };
